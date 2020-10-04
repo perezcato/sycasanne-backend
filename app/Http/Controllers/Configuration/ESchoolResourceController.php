b95e9a984201a5d91f4d18b3572ee32f4c0da358
Perez Cato <perezcatoc@gmail.com>
2020-10-04 09:37:16 +0000

    added the resource for database transformation

--- app/Http/Controllers/Configuration/ESchoolResourceController.php
+++ app/Http/Controllers/Configuration/ESchoolResourceController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers\Configuration;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Configuration\ESchoolResourceRequest;
+use App\Http\Resources\Config\ConfigResource;
 use App\Models\Configuration\ESchoolResource;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ESchoolResourceController extends Controller
 {
     public function index(ESchoolResourceRequest $request)
     {
-        $school = ESchoolResource::where('code',$request->input('code'))
-            ->select('SchoolLogo','SchoolName','Index')->get();
 
-        return response()->json([
-            'data' => $school
-        ]);
+        $school = ESchoolResource::where('TheCode', $request->input('code'))
+            ->select('MyIndex', 'TheCode', 'CompanyName', 'CompanyLogo', 'LogoURL',
+                'dbHost', 'dbName', 'dbPort', 'dbUsername', 'dbPassword')->first();
+
+        return $school ? (new ConfigResource($school))->response() :
+            response()->json(['message' => 'company not found'],Response::HTTP_NOT_FOUND);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,5 @@
 use App\Http\Controllers\Configuration\ESchoolResourceController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/config',[ESchoolResourceController::class => 'index'])->name('config.index');
-
-Route::get('/config', fn() => response(['data' => 'sinatra']));
+Route::post('/config',[ESchoolResourceController::class,'index'])->name('config.index');
 
