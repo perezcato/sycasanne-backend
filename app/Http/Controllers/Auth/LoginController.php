d7f7a2c82ab014d747bcb3a2dad508142daf1410
jonspiro <jonspiro@icloud.com>
2020-10-12 08:20:14 +0000

    added larave santum to the application

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,12 +5,22 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\Auth\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        $users = User::all()->first();
-        return response()->json($users);
+        $password = md5($request->input('data.password'));
+//        $user = User::where('UserName', $request->input('data.username'))
+//            ->where('Userpass',$password)
+//            ->get();
+
+        $user = DB::select('show tables');
+
+
+
+
+        return response()->json($user);
     }
 }
