d679499674922f57782b15766351dfe14bfb20f9
jonspiro <jonspiro@icloud.com>
2020-10-15 11:22:13 +0000

    refactored the loan repayment controller

--- app/Http/Controllers/MobileBanker/LoanRepaymentController.php
+++ app/Http/Controllers/MobileBanker/LoanRepaymentController.php
@@ -6,27 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\MobileBanker\LoanRepaymentRequest;
 use App\Models\MobileBanker\LoanRepayment;
 use App\Models\MobileBanker\LoanRepayment as Repayment;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class LoanRepaymentController extends Controller
 {
     public function index(LoanRepaymentRequest $request)
     {
-        $loanRepayment = Repayment::create([
-            'LoanRef' => $request->input('data.loan_ref'),
-            'ClientRef' => $request->input('data.client_ref'),
-            'TheRPAmt' => $request->input('data.loan_amount'),
-            'MyKyCd' => Str::uuid(),
-            'RPDescp' => $request->input('data.loan_description'),
-            'TheRPDate' => date("Y-m-d H:i:s")
-        ]);
-
-        if(!$loanRepayment){
-            return response()->json([], Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
+        LoanRepayment::addPayment($request);
         return response()->json([], Response::HTTP_CREATED);
     }
 }
--- app/Models/MobileBanker/LoanRepayment.php
+++ app/Models/MobileBanker/LoanRepayment.php
@@ -2,19 +2,33 @@
 
 namespace App\Models\MobileBanker;
 
+use App\Http\Requests\MobileBanker\LoanRepaymentRequest;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class LoanRepayment extends Model
 {
     use HasFactory;
 
     protected $connection = 'company_database';
-    protected $table = 'loanrepaymentapi';
-
+    protected $table = 'loanrepyment';
+    public $timestamps = false;
     protected $fillable = [
-        'LoanRef','ReAmount','TheDate'
+        'LoanRef','ClientRef','TheRpAmt','MyKyCd',
+        'RPDescp','TheRPDate'
     ];
 
-    public $timestamps = false;
+
+    public static function addPayment(LoanRepaymentRequest $request)
+    {
+        return LoanRepayment::create([
+            'LoanRef' => $request->input('data.loan_ref'),
+            'ClientRef' => $request->input('data.client_ref'),
+            'TheRPAmt' => $request->input('data.loan_amount'),
+            'MyKyCd' => Str::uuid(),
+            'RPDescp' => $request->input('data.loan_description'),
+            'TheRPDate' => date("Y-m-d H:i:s")
+        ]);
+    }
 }
