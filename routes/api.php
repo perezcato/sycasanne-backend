14808cc0565fe588d4f18f62e7c0469452ec8eda
Perez Cato <jonspiro@icloud.com>
2020-12-18 11:17:14 +0000

    solved request history functionality

--- routes/api.php
+++ routes/api.php
@@ -52,8 +52,8 @@ Route::middleware(['estores.locked'])->prefix('estores')->group(function(){
         Route::post('/adduser',[AuthController::class,'addUser']);
         Route::get('/products/search',[ProductsController::class,'searchProduct']);
         Route::get('/branches',[BranchesController::class,'index']);
-        Route::post('/products/{id}',[ProductsController::class,'update']);
         Route::post('/products/history',[ProductsController::class,'requestHistory']);
+        Route::post('/products/{id}',[ProductsController::class,'update']);
         Route::post('/products',[ProductsController::class,'store']);
 
     });
