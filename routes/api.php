bb3f0f1bb0ad83ca33fe8a5762f7293d6644f0b9
Cato Perez <perezcatoc@gmail.com>
2021-08-31 03:45:38 +0000

    added client search

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,7 @@ Route::middleware(['db'])->group(function(){
 
     Route::prefix('/staff')->group(function (){
         Route::post('/login',[StaffController::class,'login']);
-        Route::get('/client/search',[CompanyClient::class,'searchClients']);
+        Route::get('/client/search',[StaffController::class,'searchClients']);
     });
 
     Route::post('/unlock-device', function (){
